e1cb8875b851834e4717f4ddc21d9f9591db7480
TheCommCraft <simon.c.gilde@gmail.com>
2025-07-08 11:39:40 +0200

    fixed

--- gameworkshop/grid.py
+++ gameworkshop/grid.py
@@ -7,6 +7,7 @@ class Grid(game_object.GameObject):
     height: int
     tiles: list[list[bool]]
     tile_size: int = 32
+    value: bool | None
     def __init__(self, width: int, height: int):
         self.render_target = pygame.Surface((self.tile_size * width, self.tile_size * height))
         self.width = width
@@ -15,15 +16,29 @@ class Grid(game_object.GameObject):
     
     def __setitem__(self, item: tuple[int, int], value: bool):
         x, y = item
-        self.tiles[x][y] = value
+        try:
+            self.tiles[x][y] = value
+        except IndexError:
+            return
         pygame.draw.rect(self.render_target, (0, 0, 255 * value), (x * self.tile_size, y * self.tile_size, self.tile_size, self.tile_size))
     
     def __getitem__(self, item: tuple[int, int]) -> bool:
         x, y = item
-        return self.tiles[x][y]
+        try:
+            return self.tiles[x][y]
+        except IndexError:
+            return
     
     def draw(self, canvas):
         canvas.blit(self.render_target, self.position)
     
     def update(self):
-        pass
+        if pygame.mouse.get_pressed()[0]:
+            x, y = pygame.mouse.get_pos()
+            x = x // 32
+            y = y // 32
+            if self.value is None:
+                self.value = not self[x, y]
+            self[x, y] = self.value
+        else:
+            self.value = None
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import gameworkshop
 import gameworkshop.game_state
 import gameworkshop.grid
+import gameworkshop.consts
 
 gs = gameworkshop.game_state.GameState()
 gs.init()
-obj = gameworkshop.grid.Grid(10, 10)
+obj = gameworkshop.grid.Grid(gameworkshop.consts.SCREEN_WIDTH // 32, gameworkshop.consts.SCREEN_HEIGHT // 32)
 gs.add_obj(obj)
 gs.start()
