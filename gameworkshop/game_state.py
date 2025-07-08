f313f5130faa2fac2fe6b83a0c0d7dca51c289c8
TheCommCraft <simon.c.gilde@gmail.com>
2025-07-08 10:54:44 +0200

    consts and GameState.start

--- gameworkshop/game_state.py
+++ gameworkshop/game_state.py
@@ -1,7 +1,15 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 import pygame
+from . import consts
 
 @dataclass
 class GameState:
-    pass
+    canvas: pygame.Surface
+    running: bool
+    
+    def start(self):
+        self.canvas = pygame.display.set_mode((consts.SCREEN_WIDTH, consts.SCREEN_HEIGHT))
+        self.running = True
+        while self.running:
+            pass
